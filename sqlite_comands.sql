88a356bc3228e618af69031bf183da2254f33e5e
BnykDeda22 <oleyalex-2003@mail.ru>
2023-02-19 14:29:47 +0300

    Create linked tables

--- sqlite_comands.sql
+++ sqlite_comands.sql
@@ -1,14 +1,26 @@
+CREATE TABLE genres(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    genre VARCHAR(255) NOT NULL
+);
+
+INSERT INTO genres(genre)
+    VALUES ('Роман'),
+           ('Повесть'),
+           ('Фэнтезийная литература');
+
+
 CREATE TABLE books(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title VARCHAR(255) NOT NULL,
+    genre_id INTEGER,
     release_ INT,
-    author VARCHAR(255) NOT NULL
+    author VARCHAR(255) NOT NULL,
+    FOREIGN KEY (genre_id) REFERENCES genres(id)
 );
 
-INSERT INTO books(title, release_, author)
-    VALUES ('Евгений Онегин', 1833, 'Пушкин А.С.'),
-           ('Мы', 1988, 'Замятин Е.И.'),
-           ('Тарас Бульба', NULL, 'Н.В. Гоголь'),
-           ('Гарри Поттер и философский камень', 1997, 'Дж. К. Роулинг'),
-           ('Капитанская дочка', Null, 'Пушкин А.С.')
-
+INSERT INTO books(title, release_, author ,genre_id)
+    VALUES ('Евгений Онегин', 1833, 'Пушкин А.С.', 1),
+           ('Мы', 1988, 'Замятин Е.И.', 1),
+           ('Тарас Бульба', NULL, 'Н.В. Гоголь', 2),
+           ('Гарри Поттер и философский камень', 1997, 'Дж. К. Роулинг', 3),
+           ('Капитанская дочка', Null, 'Пушкин А.С.', NULL);
